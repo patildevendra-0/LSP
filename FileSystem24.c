b3ae73d788e873ff8ca0c98edbbaeaaa1a4c211d
patildevendra-0 <patildev2001@gmail.com>
2023-09-04 09:46:05 +0530

    new

--- FileSystem24.c
+++ FileSystem24.c
@@ -1,1 +1,31 @@
-      
+#include<stdio.h>
+#include<dirent.h>
+
+int main(int argc,char *argv[])
+{   
+    char FilePath[30];
+
+    DIR *dp = NULL;
+    struct dirent *entry = NULL;
+
+    if(argc !=2)
+    {
+        printf("INSUFFICIENT ARGUMENTS..\n");
+        return -1;
+    }
+
+    dp = opendir(argv[1]);
+    if(dp == NULL)
+    {
+        printf("UNABLE TO OPEN DIRECTORY...\n");
+        return -1;
+    }
+
+    while((entry = readdir(dp))!=NULL)
+    {
+        sprintf(FilePath,"%s/%s",argv[1],entry->d_name);
+        printf("%s\n",FilePath);
+    }
+
+    return 0;
+}
